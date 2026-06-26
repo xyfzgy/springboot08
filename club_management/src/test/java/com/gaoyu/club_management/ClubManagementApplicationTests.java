870411f3a2d56781f26674dd73401d2be0a48612
gaoyu <3508573733@qq.com>
2026-06-26 18:52:53 +0800

    合并 club_management 项目到 springboot08 仓库

---
+++ club_management/src/test/java/com/gaoyu/club_management/ClubManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaoyu.club_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
